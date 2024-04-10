053ead03f130c855a5685d401c67c15dccf22c14
fendro <tristan.schmitt@epitech.eu>
2024-04-10 15:02:15 +0200

    Setup anew

--- client/src/main.js
+++
@@ -1,9 +0,0 @@
-import "./assets/main.css";
-
-import { createApp } from "vue";
-import App from "./App.vue";
-
-import { startGame } from "../../../Connect4/dist/index.js";
-
-createApp(App).mount("#app");
-startGame();
